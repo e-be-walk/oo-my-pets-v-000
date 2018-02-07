ef92b34b635c82994bba71639d5636410a3140de
Erin Walker <erinb0317@hotmail.com>
2018-02-07 21:39:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,6 +41,16 @@ class Owner
     @pets[:dogs] << dog
   end
 
+  def walk_dogs
+    @pets[:dogs].each{|dog| dog.mood = "happy"}
+  end
 
+  def play_with_cats
+    @pets[:cats].each{|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each{|fish| fish.mood = "happy"}
+  end
 
 end
