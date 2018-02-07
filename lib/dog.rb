73e0140085cc1f41f8825081ee4b772f147b0efa
Erin Walker <erinb0317@hotmail.com>
2018-02-07 21:20:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  # code goes here
-  attr_reader :dog
+  attr_accessor :mood
+  attr_reader :name
 
-  def initialize(name)
+  def initialize(name, mood="nervous")
     @name = name
+    @mood = mood
   end
-
 end
