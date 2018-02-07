e7bf34b4030424216ed4d22bb877d510d7a6bad3
Erin Walker <erinb0317@hotmail.com>
2018-02-07 21:43:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,4 +62,8 @@ class Owner
     @pets[:fishes] = []
   end
 
+  def list_pets
+    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+
 end
