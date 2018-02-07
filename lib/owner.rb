775c32ff00c39c98a210483f49717d0a15f354fe
Erin Walker <erinb0317@hotmail.com>
2018-02-07 21:31:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -14,12 +14,12 @@ class Owner
     @@all
   end
 
-  def self.count
-    @all.size
-  end
-
   def self.reset_all
     @@all = []
+  end
+
+  def self.count
+    @all.size
   end
 
   def say_species
